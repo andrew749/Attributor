f9e7915d888dd74f5c888a18e4a4a3ea58489359
Andrew Codispoti <andrewcodispoti@Andrews-Mac-mini.local>
2014-12-27 12:25:59 -0500

    Working attributor for stanford iphone dev course.

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Andrew Codispoti on 2014-12-23.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+@property(nonatomic,strong)NSAttributedString *textToAnalyze;
+@end
